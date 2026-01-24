e45a1ec5dcb0524fefb8b516aac65fd18b8f19b7
calm1205 <nagi.moriyama@mntsq.com>
2026-01-24 19:11:27 +0900

    Consolidate config files into base.zsh
    Merge aliases.zsh, exports.zsh, keybindings.zsh into base.zsh

--- dot_config/zsh/base.zsh
+++ dot_config/zsh/base.zsh
@@ -1,3 +1,13 @@
+export EDITOR=nvim
+export PYTHONDONTWRITEBYTECODE=1
+
+# alias
+alias ls='ls --color=auto'
+alias cd='z'
+alias cdi='zi'
+alias cat='gat'
+alias vim='nvim'
+
 # history: 入力中に矢印上下で前方一致のhistry検索
 bindkey '^[[A' history-beginning-search-backward
 bindkey '^[[B' history-beginning-search-forward
--- dot_config/zsh/exports.zsh
+++
@@ -1,6 +0,0 @@
-export EDITOR=nvim
-
-# uv
-export PYTHONDONTWRITEBYTECODE=1
-export PATH="$PATH:$HOME/.local/bin"
-
