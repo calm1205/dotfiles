254664d8e974890e88f24b4611a6c637e6ec1a55
calm1205 <nagi.moriyama@mntsq.com>
2026-01-24 20:46:28 +0900

    Add Neovim configuration

---
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,34 @@
+return {
+    'nvim-telescope/telescope.nvim', version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        -- optional but recommended
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    config = function()
+      require('telescope').setup{
+        defaults = {
+          file_ignore_patterns = {
+            "^%.git/",
+            "node_modules/",
+            "%.venv/",
+          },
+          vimgrep_arguments = {
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+            '--hidden',
+          },
+        },
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+        },
+      }
+    end
+}
