cc7a5871a978fb0e1883aae047fca936b4754062
calm1205 <authentic.nagi@gmail.com>
2026-01-25 12:50:47 +0900

    feat: install treesitter

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,9 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
-  build = ':TSUpdate'
+  build = ':TSUpdate',
+  opts = {
+    ensure_installed = { "lua", "python", "typescript", "rust", "html", "css" },
+    auto_install = true,
+  }
 }
