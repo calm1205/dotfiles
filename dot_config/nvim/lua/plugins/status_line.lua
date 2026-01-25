c0b1dd7cb2394409dd70dbc1810ad787db48d87f
calm1205 <authentic.nagi@gmail.com>
2026-01-25 13:13:38 +0900

    feat: lualineの設定を整理

--- dot_config/nvim/lua/plugins/status_line.lua
+++ dot_config/nvim/lua/plugins/status_line.lua
@@ -1,12 +1,19 @@
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {
-        sections = {
-            lualine_c = {
-                { 'filename', path = 3 },
+    config = function()
+        require('lualine').setup({
+            options = {
+                theme = 'auto',
             },
-            lualine_x = { 'filetype' },
-        },
-    },
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch', 'diff', 'diagnostics' },
+                lualine_c = { 'filename' },
+                lualine_x = { 'filetype' },
+                lualine_y = {},
+                lualine_z = { 'location' },
+            },
+        })
+    end,
 }
