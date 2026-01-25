557d937987dcb5a26487d52afd180ed5799d0443
calm1205 <authentic.nagi@gmail.com>
2026-01-25 12:55:33 +0900

    feat: ステータスラインにフルパスを表示

--- dot_config/nvim/lua/plugins/status_line.lua
+++ dot_config/nvim/lua/plugins/status_line.lua
@@ -3,6 +3,9 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
         sections = {
+            lualine_c = {
+                { 'filename', path = 3 },
+            },
             lualine_x = { 'filetype' },
         },
     },
