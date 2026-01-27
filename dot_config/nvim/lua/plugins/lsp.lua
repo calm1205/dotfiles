55a13f0007d51873aecb797490064c90f569bade
calm1205 <nagi.moriyama@mntsq.com>
2026-01-27 10:58:47 +0900

    fix: vue-typescript-pluginのパスをプロジェクトローカルに変更

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -59,7 +59,7 @@ return {
         plugins = {
           {
             name = '@vue/typescript-plugin',
-            location = '/opt/homebrew/lib/node_modules/@vue/typescript-plugin',
+            location = vim.fn.getcwd() .. '/node_modules/@vue/typescript-plugin',
             languages = { 'vue' },
           },
         },
