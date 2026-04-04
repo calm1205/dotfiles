9e163409bda57823fa9005c9d2da8a6f2654491d
Nagi Moriyama <nagi.moriyama@MacBook-Pro.local>
2026-04-04 16:30:16 +0900

    feat(npm): oxlint/oxfmtのグローバルインストール追加

--- run_once_03_npm_install.sh
+++ run_once_03_npm_install.sh
@@ -3,4 +3,4 @@
 mise install node@lts
 mise use -g node@lts
 
-npm install -g @vue/language-server @vue/typescript-plugin typescript-language-server
+npm install -g @vue/language-server @vue/typescript-plugin typescript-language-server oxlint oxfmt
