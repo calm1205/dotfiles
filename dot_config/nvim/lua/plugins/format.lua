0ea73a4e4e78bf37308aa70c0490c940c1875494
Nagi Moriyama <nagi.moriyama@MacBook-Pro.local>
2026-04-04 16:45:18 +0900

    fix(nvim): conform.nvimのkeys方式移行と全filetype対応

--- dot_config/nvim/lua/plugins/format.lua
+++ dot_config/nvim/lua/plugins/format.lua
@@ -1,28 +1,24 @@
 return {
   "stevearc/conform.nvim",
-  ft = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
-  config = function()
-    local conform = require("conform")
-
-    conform.setup({
-      formatters_by_ft = {
-        javascript = { "oxfmt" },
-        javascriptreact = { "oxfmt" },
-        typescript = { "oxfmt" },
-        typescriptreact = { "oxfmt" },
-        vue = { "oxfmt" },
+  keys = {
+    {
+      "<Leader>of",
+      function()
+        require("conform").format({ async = true })
+      end,
+      desc = "Format current file",
+    },
+  },
+  opts = {
+    formatters_by_ft = {
+      ["*"] = { "oxfmt" },
+    },
+    formatters = {
+      oxfmt = {
+        command = "oxfmt",
+        args = { "--stdin-filepath", "$FILENAME" },
+        stdin = true,
       },
-      formatters = {
-        oxfmt = {
-          command = "oxfmt",
-          args = { "--stdin-filepath", "$FILENAME" },
-          stdin = true,
-        },
-      },
-    })
-
-    vim.keymap.set("n", "<Leader>of", function()
-      conform.format({ async = true })
-    end, { desc = "Format current file" })
-  end,
+    },
+  },
 }
